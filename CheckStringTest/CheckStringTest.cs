bfee9591c9d57ff0febe731ad6101dbafa1d7f2d
Dinu01 <dchandrapathak01@gmail.com>
2018-07-25 22:30:15 +0530

    Assert.AreEqual Change

--- CheckStringTest/CheckStringTest.cs
+++ CheckStringTest/CheckStringTest.cs
@@ -17,21 +17,21 @@ namespace CheckStringTest
         public void CheckFirstPosition()
         {
             var checkString = new CheckStringPosition().CheckPosition("this is string");
-            Assert.Equals("Hi", checkString);
+            Assert.AreEqual("Hi", checkString);
         }
 
         [TestMethod]
         public void CheckMiddlePosition()
         {
             var checkString = new CheckStringPosition().CheckPosition("is this string");
-            Assert.Equals("test pass", checkString);
+            Assert.AreEqual("test pass", checkString);
         }
 
         [TestMethod]
         public void CheckLastPosition()
         {
             var checkString = new CheckStringPosition().CheckPosition("string is this");
-            Assert.Equals("Valid", checkString);
+            Assert.AreEqual("Valid", checkString);
         }
     }
 }
--- StringPositionTestInTdd/CheckStringPosition.cs
+++ StringPositionTestInTdd/CheckStringPosition.cs
@@ -33,7 +33,7 @@ namespace StringPositionTestInTdd
                    }
                }
            }
-           return "";
+           return null;
        }
     }
 }
